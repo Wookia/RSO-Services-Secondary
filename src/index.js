0777b853bcc82a4a20a00ee5a46d5a1ef0ad924b
Wookia <lukasz@butryn.eu>
2018-02-28 20:30:33 +0100

    adding 'Access-Control-Allow-Origin'

--- src/index.js
+++ src/index.js
@@ -2,6 +2,19 @@ const express = require('express');
 
 const app = express();
 
+app.use(function (req, res, next) {
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    next();
+});
+
 app.get('/secondary', (req, res) => {
     res.json({
         responseFrom: "secondary"
